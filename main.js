2b87c079f5e0fbd8b192c1ad50c8e3a1871c2448
ehsan gholami kenari <ehsan1112@live.com>
2017-03-13 19:34:29 +0330

    update ...

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow
-var settingWindow
+let mainWindow
+let settingWindow
 
 function createWindow () {
   // Create the browser window.
@@ -20,8 +20,13 @@ function createWindow () {
     },*/
     width: 1024, 
     height: 768,
-    frame: true
+    frame: true,
+    show: true
   })
+
+  /*mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })*/
 
   //mainWindow.setMenu(null)
 
@@ -41,19 +46,20 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
-    settingWindow = null
+    //settingWindow = null
   })
 
-  mainWindow.on('move', function () {
+  /*mainWindow.on('show', function () {
+    console.log(mainWindow)
     console.log(settingWindow)
-  })
+  })*/
   
   // mainWindow.once('ready-to-show', () => {
-//     settingWindow = new BrowserWindow({parent: mainWindow, modal: true, show: false})
-//     settingWindow.loadURL('https://github.com')
-//     settingWindow.show()
+  //   settingWindow = new BrowserWindow({parent: mainWindow, modal: true, show: false})
+  //   settingWindow.loadURL('https://github.com')
+  //   settingWindow.show()
 
-//   })
+  //})
 
 /*const settingBtn = document.getElementById('settingBtn')
 
@@ -76,7 +82,11 @@ alert('ssssssssssssssssssssssssssssssssss');
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow()
+
+  
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -97,3 +107,8 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+global.sharedObject = {
+  mainWindow: mainWindow
+}
--- setting-windows.js
+++ setting-windows.js
@@ -3,12 +3,19 @@ const path = require('path')
 
 const settingBtn = document.getElementById('settingBtn')
 
-settingBtn.addEventListener('click', function (event) {
-  //alert('ssssssssssssssssssssssssssssssssss');
-  
+let mainWindow = require('electron').remote.getGlobal('sharedObject').mainWindow
+
+settingBtn.addEventListener('click', function (event) { 
   const modalPath = path.join('file://', __dirname, 'modal.html')
-  settingWindow = new BrowserWindow({ width: 800, height: 600, /*parent: mainWindow,*/ modal: true, frame: false })
-  settingWindow.on('close', function () { settingWindow = null })
+  settingWindow = new BrowserWindow({ width: 800, height: 600, parent: mainWindow, modal: true, show: false })
+  /*settingWindow.on('close', function () { 
+    alert("Are u ok 1 ?")
+    settingWindow = null 
+  })*/
   settingWindow.loadURL(modalPath)
+  /*settingWindow.on('closed', function () {
+    alert("Are u ok 2 ?")
+    settingWindow = null
+  })*/
   settingWindow.show()
 })
